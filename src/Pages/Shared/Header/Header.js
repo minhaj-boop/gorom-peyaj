4c8945c9371a3009bac550c1a38096381c6a0575
minhaj-boop <imminhajj@gmail.com>
2022-01-16 22:25:35 +0600

    small changes

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <>
-            <Navbar sticky="top" bg="dark" variant="dark" collapseOnSelect expand="lg">
+            <Navbar sticky="top" bg="white" variant="light" collapseOnSelect expand="lg">
                 <Container>
                     <Navbar.Brand href="#home">GCM</Navbar.Brand>
                     <Navbar.Toggle />
                     <Navbar.Collapse className="justify-content-end">
-                        <Nav.Link as={Link} to="/home#home">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/home#services">Services</Nav.Link>
-                        <Nav.Link as={Link} to="/home#experts">Experts</Nav.Link>
+                        <Nav.Link bg="dark" as={Link} to="/home#home">Home</Nav.Link>
+                        <Nav.Link bg="dark" as={Link} to="/home#services">Services</Nav.Link>
+                        <Nav.Link bg="dark" as={Link} to="/home#experts">Experts</Nav.Link>
                         {/* {user?.email ?
                             <Button onClick={logOut} variant="light">Logout</Button> :
                             <Nav.Link as={Link} to="/login">Login</Nav.Link>
